ae58f577c40244b4faf1a0b2d918d5ee09b2cfa1
Le Duy Thuc <leduythuc.cs@gmail.com>
2022-01-28 14:32:18 +0700

    Add DFS simulation

---
+++ algorithms/Graph/DFS.js
@@ -0,0 +1,23 @@
+import BaseGraph from "./BaseGraph";
+import { CELL_STATUS } from '../../components/components/Graph/Containts'
+
+class DFS extends BaseGraph {
+    search() {
+        this.dfs(...this.start);
+    }
+    dfs(x, y) {
+        if (x == this.end[0] && y == this.end[1]) {
+            return true;
+        }
+        this.grid[x][y] = CELL_STATUS.visited;
+        this.top = [x, y]
+        this.add_step();
+        for (let [u, v] of this.getNeighbor(x, y)) if (this.not_visited(u, v)) {
+            if (this.dfs(u, v))
+                return true;
+        }
+        return false;
+    }
+}
+
+export default DFS
--- components/components/Graph/Containts.js
+++ components/components/Graph/Containts.js
@@ -1,4 +1,6 @@
 export const CELL_STATUS = {
+    open: 2,
+    visited: 1,
     blank: 0,
     block: -1,
     start: -2,
