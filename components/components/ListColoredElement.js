cce7196385399123b885e14d7b63eebc09eb9ea5
neddie <tranduytienkh@gmail.com>
2022-01-28 13:37:06 +0700

    refactor

--- components/components/ListColoredElement.js
+++ components/components/ListColoredElement.js
@@ -1,38 +1,45 @@
+import React from 'react';
 import { View } from 'react-native';
 import { Dimensions } from 'react-native';
+import { Animated } from 'react-native';
 
 const window_width = Dimensions.get('window').width;
 
-function ListColoredElement({ array, sorted, swapping, comparing, height, style}) {
-    return (
-        <View style={style}>
-            {array.map((val, i) => {
-                var bg = 'blue';
-                if (sorted.includes(i)) {
-                    bg = 'green';
-                }
-                if (swapping.includes(i)) {
-                    bg = 'purple'
-                }
-                if (comparing.includes(i)) {
-                    bg = 'red'
-                }
-                
-                let frame_padding = 10,
-                    block_margin = 5,
-                    block_width = Math.round((window_width - 2 * frame_padding - (array.length + 1) * block_margin) / array.length),
-                    block_height = Math.round(val * height / Math.max(...array));
-                
-                var style = {
-                    width: block_width,
-                    height: block_height,
-                    backgroundColor: bg,
-                    marginLeft: block_margin,
-                }
-                return <View key={i} style={style}></View>
-            })}
-        </View>
-    );
-}
+export class ListColoredElement extends React.Component {
+    constructor(props){
+        super(props);
+    }
 
-export default ListColoredElement
+    render(){
+        return (
+            <View style={this.props.style}>
+                {
+                    this.props.array.map((val, i) => {
+                        let bg = 'blue';
+                        if (this.props.sorted.includes(i)) {
+                            bg = 'green';
+                        }
+                        if (this.props.swapping.includes(i)) {
+                            bg = 'purple'
+                        }
+                        if (this.props.comparing.includes(i)) {
+                            bg = 'red'
+                        }
+                        
+                        let frame_padding = 10,
+                            block_margin = 5,
+                            block_width = Math.round((window_width - 2 * frame_padding - (this.props.array.length + 1) * block_margin) / this.props.array.length),
+                            block_height = Math.round(val * this.props.height / Math.max(...this.props.array));
+                        
+                        let style = {
+                            width: block_width,
+                            height: block_height,
+                            backgroundColor: bg,
+                            marginLeft: block_margin,
+                        }
+                        return <View key={i} style={style}></View>
+                })}
+            </View>
+        );
+    }
+};
